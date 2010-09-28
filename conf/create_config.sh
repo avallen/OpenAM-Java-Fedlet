04f3b5f07db236ea8ae74b804f0799303f1c8bd0
avallen <andreas.vallen@gmail.com>
2010-09-28 19:06:27 +0200

    move conf files from webapp to top-level directory.
    We will not use the fedlet autoconfiguration mode anymore as this depends
    on the fedlet zip being pre-configured by OpenAM.

--- conf/create_config.sh
+++ conf/create_config.sh
@@ -2,6 +2,8 @@
 
 if [ $# -ne 2 ]; then
     echo "please provide the IDP and SP base-URLs as arguments to the script."
+    echo "Example:"
+    echo "$0 http://test-sso.lexware.de:8080/auth http://localapp.lexware.de:5080/fedlet"
     exit 1
 fi
 
@@ -13,7 +15,7 @@ echo "http://test-sso.lexware.de:8080/auth  =>  $IDP_URL"
 echo "http://localapp.lexware.de:5080/fedlet  =>  $SP_URL"
 
 for TEMPLATE_FILE in *.template.xml; do
-    RESULT_FILE=${TEMPLATE_FILE##.template.xml}.xml
+    RESULT_FILE=${TEMPLATE_FILE%%.template.xml}.xml
     echo "Creating file $RESULT_FILE from $TEMPLATE_FILE."
-    sed '|http://test-sso.lexware.de:8080/auth|$IDP_URL|g' |  sed '|http://localapp.lexware.de:5080/fedlet|$SP_URL|g' > ${CONF_FILE##.template.xml}.xml
+    cat $TEMPLATE_FILE | sed "s|http://test-sso.lexware.de:8080/auth|$IDP_URL|g" |  sed "s|http://localapp.lexware.de:5080/fedlet|$SP_URL|g" > $RESULT_FILE
 done
